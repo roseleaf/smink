02b2f6d52667b57d09b550f9918b0a2a7ec0d22d
roseleaf <pteroline@gmail.com>
2012-07-31 15:18:20 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smink::Application.config.secret_token = '4ef7aa9dcde674be6c3f10fe616d3948e5f65a93999f4739e2ea8339c168f44ccf2d519f9e413c4d4633ba29c2d64552a35d2df51b7b1e6af6d8e7d0287ad1bb'
